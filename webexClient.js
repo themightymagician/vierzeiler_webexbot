e7d38ec595c586d610e9e114723a2d485e8e3b6a
themightymagician <150191304+themightymagician@users.noreply.github.com>
2026-02-10 01:48:25 +0100

    Update webexClient.js

--- webexClient.js
+++ webexClient.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const webex = axios.create({
   baseURL: 'https://webexapis.com/v1',
   headers: {
-    Authorization: `Bearer ${process.env.WEBEX_BOT_TOKEN}`,
+    Authorization: `${process.env.WEBEX_BOT_TOKEN}`,
     'Content-Type': 'application/json'
   }
 });
