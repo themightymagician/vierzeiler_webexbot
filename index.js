d1122e1570ae2d6cbef6305a3d62d1566e135ee3
themightymagician <150191304+themightymagician@users.noreply.github.com>
2026-02-10 01:41:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-require('dotenv').config();
-const axios = require('axios');
 const pollMessages = require('./listener');
-
-// Erstelle einen Webex-Client
-const webex = axios.create({
-  baseURL: 'https://webexapis.com/v1',
-  headers: {
-    Authorization: process.env.WEBEX_BOT_TOKEN,
-    'Content-Type': 'application/json'
-  }
-});
-
-module.exports = webex;
 
 // Polling starten
 console.log('Webex-Bot startet...');
@@ -20,4 +7,3 @@ pollMessages()
     console.error('Fehler beim Starten des Pollings:', err);
     process.exit(1); // Railway kann den Prozess neu starten
   });
-
